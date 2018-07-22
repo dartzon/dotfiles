037e540ff8b2ac410c3173e99d2bf284f04fc13b
Othmane AIT EL CADI <dartzon@gmail.com>
2018-07-22 16:12:50 +0100

    Add ignored directories to projectile

--- emacs/config/setup-projectmanagement.el
+++ emacs/config/setup-projectmanagement.el
@@ -8,6 +8,10 @@
 (setq projectile-enable-caching t)
 ;; Disable remote file exists cache
 (setq projectile-file-exists-remote-cache-expire nil)
+
+;; Ignore the following folders.
+(setq projectile-globally-ignored-directories '(".idea" ".ensime_cache" ".eunit" ".git" ".hg" ".fslckout" "_FOSSIL_" ".bzr" "_darcs" ".tox" ".svn" ".stack-work" ".cquery_cached_index"))
+
 ;; Ignore the following files extensions.
 (setq projectile-globally-ignored-file-suffixes '(".o" ".a" ".so" ".dll" ".exe"))
 

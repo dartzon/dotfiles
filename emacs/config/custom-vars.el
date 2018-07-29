710f0642450e64eabd6a4767e17727dd2df42dc0
Othmane AIT EL CADI <dartzon@gmail.com>
2018-07-29 11:10:08 +0100

    Add markdown package for emacs

--- emacs/config/custom-vars.el
+++ emacs/config/custom-vars.el
@@ -8,7 +8,7 @@
  '(org-agenda-files nil)
  '(package-selected-packages
    (quote
-    (flycheck-kotlin kotlin-mode cmake-mode json-reformat json-snatcher makey markdown-mode memoize nlinum page-break-lines popup shut-up nlinum-hl company-lsp lsp-ui cquery lsp-mode company-ycmd ## modern-cpp-font-lock counsel-etags ivy dockerfile-mode hackernews company-irony-c-headers flycheck-rtags company-irony company-rtags company-c-headers cmake-ide irony company auto-complete-clang rtags yasnippet flycheck-clang-tidy flycheck json-mode cmake-font-lock clang-format whitespace-cleanup-mode column-enforce-mode smartparens evil-nerd-commenter web-mode rainbow-mode erc-hl-nicks dim zoom smex ido-completing-read+ projectile bm discover-my-major anzu multiple-cursors highlight-symbol xclip zenburn-theme spaceline pallet dashboard bind-key))))
+    (markdown-toc flycheck-kotlin kotlin-mode cmake-mode json-reformat json-snatcher makey markdown-mode memoize nlinum page-break-lines popup shut-up nlinum-hl company-lsp lsp-ui cquery lsp-mode company-ycmd ## modern-cpp-font-lock counsel-etags ivy dockerfile-mode hackernews company-irony-c-headers flycheck-rtags company-irony company-rtags company-c-headers cmake-ide irony company auto-complete-clang rtags yasnippet flycheck-clang-tidy flycheck json-mode cmake-font-lock clang-format whitespace-cleanup-mode column-enforce-mode smartparens evil-nerd-commenter web-mode rainbow-mode erc-hl-nicks dim zoom smex ido-completing-read+ projectile bm discover-my-major anzu multiple-cursors highlight-symbol xclip zenburn-theme spaceline pallet dashboard bind-key))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
